5ed0edbe62dc39a74f8709c0356017734e3f4dae
nazunamoe <jpg3927@gmail.com>
2015-12-14 12:00:37 +0900

    add v490 p490l

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter v400 v410, $(TARGET_DEVICE)),)
+ifneq ($(filter v400, v410, p490l, v490, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
